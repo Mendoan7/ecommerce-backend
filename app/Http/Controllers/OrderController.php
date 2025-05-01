c054678110ac7424a6bef70b098c5485659ab862
Mendoan7 <ardiansyahbisma7@gmail.com>
2025-05-02 00:01:59 +0700

    add api list order, feature search and last order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product\Product;
+use App\ResponseFormatter;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $query = auth()->user()->orders()->with([
+            'seller',
+            'address',
+            'items',
+            'lastStatus'
+        ]);
+
+        if (request()->last_status){
+            $query->whereHas('lastStatus', function($subQuery){
+                $subQuery->where('status', request()->last_status);
+            });
+        }
+
+        if (request()->search){
+            $query->whereHas('seller', function($subQuery){
+                $subQuery->where('store_name', 'LIKE', '%' . request()->search . '%');
+            })->orWhere('invoice_number', 'LIKE', '%' . request()->search . '%');
+
+            $productIds = Product::where('name', 'LIKE', '%' . request()->search . '%')->pluck('id');
+            $query->orWhereHas('items', function($subQuery) use($productIds) {
+                $subQuery->whereIn('product_id', $productIds);
+            });
+        }
+
+        $orders = $query->paginate(request()->per_page ?? 10);
+
+        return ResponseFormatter::success($orders->through(function($order){
+            return $order->api_response;
+        }));
+    }
+}
