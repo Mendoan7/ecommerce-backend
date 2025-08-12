7538309fba0ff87c0db5b003369e39a76207be12
Mendoan7 <ardiansyahbisma7@gmail.com>
2025-08-12 10:04:30 +0700

    add category product and fix category item search

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -21,30 +21,71 @@ class CategorySeeder extends Seeder
                 'childs' => ['Kulkas', 'TV', 'AC'],
             ],
             [
-                'name' => 'Fashion Pria',
-                'icon' => 'category/Fashion-Pria.webp',
-                'childs' => ['Jacket', 'Kaos', 'Kemeja'],
+                'name' => 'Komputer & Laptop',
+                'icon' => 'category/Komputer-Laptop.webp',
+                'childs' => ['Laptop', 'Mouse', 'Keyboard'],
             ],
             [
-                'name' => 'Fashion Wanita',
-                'icon' => 'category/Fashion-Wanita.webp',
-                'childs' => ['Dress', 'Atasan', 'Bawahan'],
-            ],
-            [
-                'name' => 'Handphone',
+                'name' => 'Handphone & Aksesoris',
                 'icon' => 'category/Handphone.webp',
                 'childs' => ['Handphone', 'Case', 'Powerbank'],
             ],
             [
-                'name' => 'Komputer & Laptop',
-                'icon' => 'category/Komputer-Laptop.webp',
-                'childs' => ['Laptop', 'Mouse', 'Keyboard'],
+                'name' => 'Fashion Pria',
+                'icon' => 'category/Fashion-Pria.webp',
+                'childs' => ['Jacket', 'Kaos', 'Kemeja', 'Celana'],
+            ],
+            [
+                'name' => 'Sepatu Pria',
+                'icon' => 'category/Sepatu.webp',
+                'childs' => ['Sneakers', 'Running', 'Sandal'],
+            ],
+            [
+                'name' => 'Jam Tangan',
+                'icon' => 'category/Jam-Tangan.webp',
+                'childs' => ['Jam Tangan Pria', 'Jam Tangan Wanita', 'Aksesoris Jam'],
+            ],
+            [
+                'name' => 'Kesehatan',
+                'icon' => 'category/Kesehatan.webp',
+                'childs' => ['Obat-Obatan', 'Suplemen', 'Alat Kesehatan'],
             ],
             [
                 'name' => 'Makanan & Minuman',
                 'icon' => 'category/Makanan-Minuman.webp',
                 'childs' => ['Makanan', 'Minuman'],
             ],
+            [
+                'name' => 'Perawatan & Kecantikan',
+                'icon' => 'category/Makeup.webp',
+                'childs' => ['Makeup', 'Skincare', 'Haircare'],
+            ],
+            [
+                'name' => 'Perlengkapan Rumah Tangga',
+                'icon' => 'category/Rumah-Tangga.webp',
+                'childs' => ['Peralatan Dapur', 'Perabotan Rumah', 'Dekorasi'],
+            ],
+            [
+                'name' => 'Fashion Wanita',
+                'icon' => 'category/Fashion-Wanita.webp',
+                'childs' => ['Dress', 'Atasan', 'Bawahan', 'Sepatu Wanita'],
+            ],
+            [
+                'name' => 'Fashion Anak',
+                'icon' => 'category/Fashion-Anak.webp',
+                'childs' => ['Baju Anak', 'Celana Anak', 'Sepatu Anak'],
+            ],
+            [
+                'name' => 'Ibu & Bayi',
+                'icon' => 'category/Ibu-Bayi.webp',
+                'childs' => ['Makanan Bayi', 'Perawatan Bayi', 'Mainan Bayi', 'Pakaian Bayi'],
+            ],
+            [
+                'name' => 'Tas & Aksesoris',
+                'icon' => 'category/Tas-Wanita.webp',
+                'childs' => ['Tas Pria', 'Tas Wanita', 'Aksesoris'],
+            ]
+
         ];
 
         foreach ($categories as $categoryPayload) {
